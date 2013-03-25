cff3e5895edbc8928003f5759f388b6b46aad949
Ethan Birch <ethanjbirch@gmail.com>
2013-03-25 16:31:22 -0600

    UserHash

--- Ants/src/AccessRight.java
+++ Ants/src/AccessRight.java
@@ -2,6 +2,11 @@ public class AccessRight
 {
     private String label;
     
+    public AccessRight(String label)
+    {
+    	this.label=label;	
+    }
+    
     public String getLabel()
     {
     	return label;
