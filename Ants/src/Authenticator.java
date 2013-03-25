c4fa8f1a2d0becd32028a30339963b96d3b11d16
cjthatcher <cjthatcher@gmail.com>
2013-03-25 15:53:01 -0600

    Word

--- Ants/src/Authenticator.java
+++ Ants/src/Authenticator.java
@@ -1,8 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
  public class Authenticator
  {
-     public String login(String username, String password)
+	 Map userMap = new HashMap<String, User>();
+	 
+     public String login(String userName, String password)
      {
-         //do this later
+    	 if (userMap.containsKey(userName))
+    	 {
+    		 User temp = (User) userMap.get(userName);
+    		 
+    		 if (temp.check(userName, password))
+    		 {
+    			 Session s = Session.getInstance();
+    			 String name = String.format("%s %s", temp.getFirstName(), temp.getLastName());
+    			 String time = s.getTime().toString();
+    			 String accessLevel = Integer.toString(s.getAccessLevel());
+    			 
+    			 
+    			 //successfully logged in
+    		 }
+    	 }
+    	 return "Shutup!";
 
      }
  
