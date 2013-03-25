4196953c13e94369159afe8702bc8bd49d1f3bef
Preston Lamb <preston.j.lamb@gmail.com>
2013-03-25 15:30:30 -0600

    User class getters/setters and the check function to see if the username and password combo are correct

--- Ants/src/User.java
+++ Ants/src/User.java
@@ -3,14 +3,57 @@ import java.util.List;
 
 
 public class User {
-	 String username;
-	    String password;
-	    String firstName;
-	    String lastName;
-	    List<UserGroup> groups;
-	    
-	    public boolean check(String username, String password)
-	    {
-	        //do this later
-	    }
+	private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private List<UserGroup> groups;
+    
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<UserGroup> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<UserGroup> groups) {
+		this.groups = groups;
+	}
+
+	public boolean check(String username, String password)
+    {
+        if(username == this.username && password == this.password)
+        	return true;
+        else
+        	return false;
+    }
 }
