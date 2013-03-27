9c1de9b6e922eca8b08f27e8712310c4d62792df
cjthatcher <cjthatcher@gmail.com>
2013-03-27 16:19:09 -0600

    tests

--- Ants/src/TestCase.java
+++ Ants/src/TestCase.java
@@ -6,8 +6,23 @@ import org.junit.Test;
 public class TestCase {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Test_01_authenticator_construct() {
+		
+		Authenticator a = new Authenticator();
+		
+		assertNotNull(a);
+	}
+	
+	@Test
+	public void test_02_signin_success()
+	{
+		Authenticator a = new Authenticator();
+		
+		String successString = a.login("georgeMoney", "redcoatsSuck");
+		
+		boolean itWorked = successString.startsWith("George Washington successfully logged in at");
+		
+		assertTrue(itWorked);
 	}
 
 }
