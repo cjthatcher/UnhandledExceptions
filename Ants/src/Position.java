1fc4836d82e77a40b0a236e117404439922ed0ee
Michael Hansen <michael.tabascoboy86@gmail.com>
2013-04-04 18:54:23 -0600

    Position Class definition

--- Ants/src/Position.java
+++ Ants/src/Position.java
@@ -1,4 +1,43 @@
 
-public class Position {
 
+public class Position 
+{
+	int x;
+	int y;
+	
+	public Position(int x, int y)
+	{
+		this.x = x;
+        this.y = y;
+		
+	}
+        
+	public void setPosition(int x, int y)
+	{
+        this.x = x;
+        this.y = y;
+	}
+	
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+        
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+        
+	public int getX()
+	{
+        return x;
+	}
+	
+	public int getY()
+	{
+        return y;
+	}
+	
 }
+
+
