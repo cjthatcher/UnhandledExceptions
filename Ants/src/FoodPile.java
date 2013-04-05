0d2eb94e69e5fb18d917faff17bb1b35cf7de3e0
Michael Hansen <michael.tabascoboy86@gmail.com>
2013-04-04 19:04:28 -0600

    FoodPile class definition

--- Ants/src/FoodPile.java
+++ Ants/src/FoodPile.java
@@ -1,4 +1,26 @@
 
 public class FoodPile {
-
+    
+    int foodAmount;
+    
+    public FoodPile(int amount)
+    {
+        foodAmount = amount;
+    }
+    
+    public int getFoodAmount()
+    {
+        return foodAmount;
+    }
+    
+    public void decrementFood()
+    {
+        foodAmount--;
+    }
+    
+    public void setFoodAmount(int amount)
+    {
+        foodAmount = amount;
+    }
+    
 }
