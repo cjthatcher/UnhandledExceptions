1b53acc6c5f11bd61126e95b843c798e8432a1a8
cjthatcher <cjthatcher@gmail.com>
2013-04-09 18:42:12 -0600

    update

--- Ants/src/PheromoneComparator.java
+++ Ants/src/PheromoneComparator.java
@@ -1,19 +1,18 @@
 import java.util.Comparator;
 
 
-public class PheromoneComparator implements Comparator{
+public class PheromoneComparator implements Comparator<Pheromone>{
 
 	@Override
-	public int compare(Object p1, Object p2) {
-		
-		int s1 = ((Pheromone)p1).getStrength();
-		int s2 = ((Pheromone)p2).getStrength();
+	public int compare(Pheromone o1, Pheromone o2) {
+		int s1 = o1.getStrength();
+		int s2 = o2.getStrength();
 		
 		if (s1 > s2)
 			return 1;
 		else if (s1 < s2)
 			return -1;
-	
+		
 		return 0;
 	}
 
