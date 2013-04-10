1155bc339d760860146db5a4e579127a08243713
Ethan Birch <ethanjbirch@gmail.com>
2013-04-09 18:28:21 -0600

    Updated GroundCell and Pheromone

--- Ants/src/Pheromone.java
+++ Ants/src/Pheromone.java
@@ -1,14 +1,37 @@
 
 public class Pheromone {
 	Colony colony;
+	int strength;
+	GroundCell gc;
 	
-	public Pheromone(Colony c)
+	public Pheromone(Colony c, int str, GroundCell gc)
 	{
 		this.colony = c;
+		strength=str;
+		this.gc=gc;
 	}
 	
 	public Colony getColony()
 	{
 		return colony;
+	}
+	
+	public int getStrength()
+	{
+		return strength;
+	}
+	
+	public GroundCell getLocation()
+	{
+		return gc;
+	}
+	
+	public void agePheromone()
+	{
+		--strength;
+		if(strength<=0)
+		{
+			gc.losePheromone(this);
+		}
 	}
 }
